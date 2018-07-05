dfa0b088e9cd30f760287788297652723c655cb5
Sarah Lynch <sarah@alum.rpi.edu>
2018-07-05 13:18:44 -0400

    work in progress

--- game/index.js
+++ game/index.js
@@ -1,6 +1,37 @@
 import {Map} from 'immutable'
+let board = Map();
+const initialState = {board: board,
+turn: 'X'};
+//ACTIONS
+const MOVE = 'MOVE';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+//ACTION CREATOR
+export const move = (player, positionArray) => {
+  return {
+    type: MOVE,
+    position: positionArray,
+    player: player
+  }
 }
+
+//
+
+
+export default function reducer(state = initialState, action){
+let newState = Object.assign({}, state);
+  switch (action.type){
+    case MOVE:
+    console.log("HELLO: ", action)
+    let newBoard = newState.board.setIn(action.position, action.player)
+      return {board: newBoard,
+      turn: 'X'}
+      //create if logic to change turn
+    default: return state
+
+  }
+}
+
+// export default function reducer(state, action) {
+//   // TODO
+//   return state
+// }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import inquirer from 'inquirer'
 
 import gameReducer, {move} from './game'
 import {createStore} from 'redux'
+// Create the store
+const game = createStore(gameReducer)
 
 const printBoard = () => {
   const {board} = game.getState()
@@ -14,7 +16,7 @@ const printBoard = () => {
 }
 
 const getInput = player => async () => {
-  const {turn} = game.getState()  
+  const {turn} = game.getState()
   if (turn !== player) return
   const ans = await inquirer.prompt([{
     type: 'input',
@@ -25,11 +27,10 @@ const getInput = player => async () => {
   game.dispatch(move(turn, [row, col]))
 }
 
-// Create the store
-const game = createStore(gameReducer)
+
 
 // Debug: Print the state
-// game.subscribe(() => console.log(game.getState()))
+//game.subscribe(() => console.log(game.getState()))
 
 game.subscribe(printBoard)
 game.subscribe(getInput('X'))
